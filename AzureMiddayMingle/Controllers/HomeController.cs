70b72416e31586e1f02a22249057ba057d6fbb2f
loreinn13 <loreinn13@gmail.com>
2017-11-09 01:18:41 -0500

    Got a working API string

--- AzureMiddayMingle/Controllers/HomeController.cs
+++ AzureMiddayMingle/Controllers/HomeController.cs
@@ -7,7 +7,10 @@ using AzureMiddayMingle.Models;
 using System.Configuration;
 using System.Data.SqlClient;
 using Foursquare.Api;
-
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
 
 namespace AzureMiddayMingle.Controllers
 {
@@ -52,8 +55,25 @@ namespace AzureMiddayMingle.Controllers
 
         public ActionResult GetData()
         {
+            HttpWebRequest request = WebRequest.CreateHttp("https://api.foursquare.com/v2/venues/search?client_id=GXLVM2YRQLY4BF5YAY2QN1UVDBDKYU0IL5420SJTGTI4RZ5T&client_secret=12OG4YUDEFBX32OFGYJNUHWOZUPV2JLKZG2RNKU1FPFJNV5H&intent=checkin&near=Grand%20Rapids&categoryId=4bf58dd8d48988d10a941735&radius=8047&v=20171109");
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            String ApiText = rd.ReadToEnd();
+            JObject o = JObject.Parse(ApiText);
 
-            return View("Data");
+            List<string> restaurants = new List<string>();
+
+            for (int i = 0; i < 1/*o["venues"].Count()*/; i++)
+            {
+                string input = o["venues"]["name"].ToString();
+                restaurants.Add(input);
+            }
+
+
+            ViewBag.SuggestedRestaurants = restaurants;
+
+            return View("Suggestions");
         }
     }
 }
